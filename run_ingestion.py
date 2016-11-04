db9836afec64076b1d3cc418f872e7955eceda2f
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-11-04 18:28:29 -0500

    Updated run_ingestion.py for use the new entities and new settings.conf file.

--- run_ingestion.py
+++ run_ingestion.py
@@ -1,42 +1,42 @@
 # Datacube Ingestion Scheduler
 
 from ConfigParser import ConfigParser
-from dao.Connection import Connection
-from dao.IngestTask import IngestTask
+from entities.LockFile import LockFile
+from entities.Connection import Connection
+from entities.IngestionTasks import IngestionTasks
 from exceptions import Exception
 import os, sys
 
-#PID_FILE = TO_INGEST + '/pid.lock'
 CONF_FILE = 'settings.conf'
 
 conf = ConfigParser()
 conf.read(CONF_FILE)
 
-dbconn = Connection(
-			host=conf.get('database','host'),
-			port=conf.get('database','port'),
-			name=conf.get('database','name'),
-			user=conf.get('database','user'),
-			password=conf.get('database','password')
-			)
+lockfile = LockFile(conf.get('other','lock_file'))
+if lockfile.search():
+	print 'There\'s an execution in progress'
+	sys.exit(1)
+else:
+	lockfile.write()
 
-dbconn.connect()
-itask = IngestTask(dbconn.curr_conn)
-print itask.get_completed()
+try:
+	print 'DATACUBE INGESTION SHCEDULER'
 
-#lockfile = LockFile(PID_FILE)
-#if lockfile.search():
-#	print 'There\'s an execution in progress'
-#	sys.exit(1)
-#else:
-#	lockfile.write()
+	dbconn = Connection(
+				host=conf.get('database','host'),
+				port=conf.get('database','port'),
+				name=conf.get('database','name'),
+				user=conf.get('database','user'),
+				password=conf.get('database','password')
+				)
 
-#try:
-#	print 'DATACUBE INGESTION SHCEDULER'
-#	ing_tasks = IngestionTasks()
-#	ing_tasks.search_tasks(TO_INGEST)
-#except Exception as e:
-#	print 'Error: ' + str(e)
-#	raise e
-#finally:
-#	lockfile.delete()
+	dbconn.connect()
+	itasks = IngestionTasks(dbconn.curr_conn)
+	itasks.load_scheduled()
+	print itasks.tasks
+
+except Exception as e:
+	print 'Error: ' + str(e)
+	raise e
+finally:
+	lockfile.delete()
