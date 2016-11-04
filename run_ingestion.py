2ffb8c0f337310b23b2bd1339c4a8232173c3c83
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-11-03 19:01:48 -0500

    Added connection to the database using DAOs and configuration file.

--- run_ingestion.py
+++ run_ingestion.py
@@ -1,26 +1,42 @@
 # Datacube Ingestion Scheduler
 
-from utils.LockFile import LockFile
-from utils.IngestionTasks import IngestionTasks
+from ConfigParser import ConfigParser
+from dao.Connection import Connection
+from dao.IngestTask import IngestTask
 from exceptions import Exception
 import os, sys
 
-TO_INGEST = os.environ['TO_INGEST']
-PID_FILE = TO_INGEST + '/pid.lock'
+#PID_FILE = TO_INGEST + '/pid.lock'
+CONF_FILE = 'settings.conf'
 
-lockfile = LockFile(PID_FILE)
-if lockfile.search():
-	print 'There\'s an execution in progress'
-	sys.exit(1)
-else:
-	lockfile.write()
+conf = ConfigParser()
+conf.read(CONF_FILE)
 
-try:
-	print 'DATACUBE INGESTION SHCEDULER'
-	ing_tasks = IngestionTasks()
-	ing_tasks.search_tasks(TO_INGEST)
-except Exception as e:
-	print 'Error: ' + str(e)
-	raise e
-finally:
-	lockfile.delete()
+dbconn = Connection(
+			host=conf.get('database','host'),
+			port=conf.get('database','port'),
+			name=conf.get('database','name'),
+			user=conf.get('database','user'),
+			password=conf.get('database','password')
+			)
+
+dbconn.connect()
+itask = IngestTask(dbconn.curr_conn)
+print itask.get_completed()
+
+#lockfile = LockFile(PID_FILE)
+#if lockfile.search():
+#	print 'There\'s an execution in progress'
+#	sys.exit(1)
+#else:
+#	lockfile.write()
+
+#try:
+#	print 'DATACUBE INGESTION SHCEDULER'
+#	ing_tasks = IngestionTasks()
+#	ing_tasks.search_tasks(TO_INGEST)
+#except Exception as e:
+#	print 'Error: ' + str(e)
+#	raise e
+#finally:
+#	lockfile.delete()
