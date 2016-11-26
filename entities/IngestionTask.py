d3ab305e74e50ab263150e162af93274c5374b09
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-11-25 20:54:15 -0500

    Updated entity IngestionTask.py.

--- entities/IngestionTask.py
+++ entities/IngestionTask.py
@@ -13,8 +13,12 @@ class IngestionTask():
 		
 		self._id = dao_itask['id']
 		self.state = dao_itask['state']
-		self.errors = dao_itask['errors']
+		self.comments = dao_itask['comments']
+		self.error_messages = dao_itask['error_messages']
+		self.logs = dao_itask['logs']
 		self.start_execution_date = dao_itask['start_execution_date']
 		self.end_execution_date = dao_itask['end_execution_date']
+		self.created_at = dao_itask['created_at']
 		self.updated_at = dao_itask['updated_at']
+		self.created_by_id = dao_itask['created_by_id']
 		self.storage_unit_id = dao_itask['storage_unit_id']
