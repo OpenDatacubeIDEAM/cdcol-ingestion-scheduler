553df38267e9ad5ba2f9ba7c807d60a5dbdbca1a
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-11-30 11:08:10 -0500

    Run ingestion script for each storage unit

--- run_ingestion.py
+++ run_ingestion.py
@@ -7,12 +7,16 @@ from entities.IngestionTasks import IngestionTasks
 from entities.StorageUnit import StorageUnit
 from dao.StorageUnit import StorageUnit as DAOStorageUnit
 from exceptions import Exception
-import os, sys
+import os, sys, subprocess
+from subprocess import CalledProcessError
 
 CONF_FILE = 'settings.conf'
 
 conf = ConfigParser()
 conf.read(CONF_FILE)
+
+TO_INGEST = conf.get('paths','to_ingest')
+ING_SCRIPT = conf.get('other','ing_script')
 
 lockfile = LockFile(conf.get('other','lock_file'))
 if lockfile.search():
@@ -34,12 +38,19 @@ try:
 
 	dbconn.connect()
 	itasks = IngestionTasks(dbconn.curr_conn)
-	dao_stgunit = DAOStorageUnit(dbconn.curr_conn)
 	itasks.load_scheduled()
+	dao_stgunit = DAOStorageUnit(dbconn.curr_conn)
 	for stg_unit_id in itasks.tasks:
 		stg_unit = StorageUnit(dao_stgunit.get_by_id(stg_unit_id))
-		print stg_unit.name
-		print stg_unit.root_dir + '/' + stg_unit.ingest_file
+
+		stg_to_ingest =  TO_INGEST + '/' + stg_unit.name
+		stg_conf_file = stg_unit.root_dir + '/' + stg_unit.ingest_file
+		stg_mgen_script = stg_unit.root_dir + '/' + stg_unit.metadata_generation_script
+		
+		try:
+			subprocess.check_output([ING_SCRIPT, stg_to_ingest, stg_conf_file, stg_mgen_script])
+		except CalledProcessError as cpe:
+			print 'Error running ingestion script: ' + str(cpe)
 
 except Exception as e:
 	print 'Error: ' + str(e)
