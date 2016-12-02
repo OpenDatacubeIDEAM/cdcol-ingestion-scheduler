fa3a5acfdca4601e80adb906a6c18c1cc379daaa
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-02 16:46:02 -0500

    Agregado parametro para tabls de colores en generación de thumbnails.

--- scripts/generate_thumbnails.sh
+++ scripts/generate_thumbnails.sh
@@ -4,6 +4,7 @@ STG_UNIT_FOLDER="$1"
 THUMBNAILS_FOLDER="$2"
 X_RES="$3"
 Y_RES="$4"
+COLORS="$5"
 
 mkdir -p $THUMBNAILS_FOLDER
 for file in $STG_UNIT_FOLDER/*.nc
@@ -15,7 +16,9 @@ do
 		then
 			echo "Escribiendo los thumbnails para el archivo $file y la banda ${ds##*\:}"
 			gdal_translate -a_srs EPSG:32618 -a_nodata -9999 -stats -of PNG -scale -outsize $X_RES $Y_RES $ds $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png
-			convert -transparent "#000000" $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png  $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png
+			convert -transparent "#000000" $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png $COLORS -clut $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png
+			convert $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png -background Khaki label:"${bn%.nc}.${ds##*\:}" -gravity Center -append $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png
 		fi
 	done
 done
+
