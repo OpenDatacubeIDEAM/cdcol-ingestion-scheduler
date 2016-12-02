ad80fac54e7f1c46401c2e6a4cd4b23792cc4109
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-01 22:56:46 -0500

    Actualizado DAO para actualización. Se reemplaza 'None' por NULL

--- dao/IngestTask.py
+++ dao/IngestTask.py
@@ -25,20 +25,21 @@ class IngestTask():
 		rows = cur.fetchall()
 		return rows
 
-	def update(_id, state, comments, error_messages, logs, start_execution_date, end_execution_date, created_at, updated_at, created_by_id, storage_unit_id):
+	def update(self, _id, state, comments, error_messages, logs, start_execution_date, end_execution_date, created_at, updated_at, created_by_id, storage_unit_id):
 		cur = self.conn.cursor(cursor_factory=DictCursor)
-		cur.execute('UPDATE ingest_ingesttask SET ' +
-					'state=' + state + ',' +
-					'comments="' + comments + '",' +
-					'error_messages="' + error_messages + '",' +
-					'logs="' + logs + '",' +
-					'start_execution_date=' + start_execution_date + ',' +
-					'end_execution_date=' + end_execution_date + ',' +
-					'created_at=' + created_at + ',' +
-					'updated_at=' + updated_at + ',' +
-					'created_by_id=' + created_by_id + ',' +
-					'storage_unit_id=' + storage_unit_id + ' ' +
-					'FROM ingest_ingesttask ' +
-					'WHERE id=' + _id + ';')
+		query = ('UPDATE ingest_ingesttask SET ' +
+				'state=' + str(state) + ', ' +
+				'comments=\'' + str(comments) + '\',' +
+				'error_messages=\'' + str(error_messages) + '\',' +
+				'logs=\'' + str(logs) + '\',' +
+				'start_execution_date=\'' + str(start_execution_date) + '\',' +
+				'end_execution_date=\'' + str(end_execution_date) + '\',' +
+				'created_at=\'' + str(created_at) + '\',' +
+				'updated_at=\'' + str(updated_at) + '\',' +
+				'created_by_id=' + str(created_by_id) + ',' +
+				'storage_unit_id=' + str(storage_unit_id) + ' ' +
+				'WHERE id=' + str(_id) + ';')
+		query = query.replace('\'None\'', 'NULL')
+		cur.execute(query)
 		self.conn.commit()
 		cur.close()
