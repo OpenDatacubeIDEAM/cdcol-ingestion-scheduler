d237c75c7ea67d8e08088361a2b10e371b9a7456
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-02 09:55:00 -0500

    Agregado script generate_thumbnails.sh para la generación de thumbnails.

---
+++ scripts/generate_thumbnails.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Parámetros (opcionales) Unidad de almacenamiento, resolución. 
+STG_UNIT_FOLDER="$1" 
+THUMBNAILS_FOLDER="$2"
+X_RES="$3"
+Y_RES="$4"
+
+mkdir -p $THUMBNAILS_FOLDER
+for file in $STG_UNIT_FOLDER/*.nc
+do
+	for ds in `gdalinfo $file |grep -Eo "NETCDF.*"`
+	do
+		bn=`basename $file`
+		echo "Escribiendo los thumbnails para el archivo $file y la banda ${ds##*\:}"
+		gdal_translate -a_srs EPSG:32618 -a_nodata -9999 -stats -of PNG -scale -outsize $X_RES $Y_RES $ds $THUMBNAILS_FOLDER/${bn%.nc}.${ds##*\:}.png
+	done
+done
