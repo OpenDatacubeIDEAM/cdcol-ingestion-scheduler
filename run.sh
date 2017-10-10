fece71f09e9b9c2fb457f5a5920ac1ce71cadfe1
Maria Paula Mancipe <mp.mancipe10@uniandes.edu.co>
2017-10-10 01:26:22 +0000

    Modificación en variables

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PYTHON='anaconda_python_bin'
+PYTHON='/home/cubo/anaconda2/bin/python'
 
 echo "$(date) RUNNING INGESTION SCHEDULER"
 source /home/cubo/.bashrc
-cd /home/cubo/ingestion
+cd /home/cubo/ingestion-scheduler
 $PYTHON run_ingestion.py
 echo "$(date) DONE"
